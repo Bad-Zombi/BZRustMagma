e2297c9e0e9292b732109619cfa0b06b54bc21d3
John Lund <git@badzombi.com>
2014-04-26 23:28:21 -0400

    core: added check for default settings before sending data to web. Users: added users plugin

--- BZ0core/BZ0core.js
+++ BZ0core/BZ0core.js
@@ -1,217 +1,231 @@
 // Zero Core : Required for all BadZombi Magma plugins.
-
 var plugin = {};
 	plugin.name = "BadZombi Zero Core";
 	plugin.author = "BadZombi";
 	plugin.version = "0.1";
 
-function getFilename(){
-	var filename = plugin.name;
-    return filename.replace(" ", "_");
-}
 
-function createConfig(data){
-	try {
+// Shared stuff:
 
-		var filename = getFilename(plugin);
-
-		Util.ConsoleLog("Config does not exist... creating " + filename + ".ini", true);
-
-	    Plugin.CreateIni(filename);
-	    var newConf = Plugin.GetIni(filename);
-
-	    for (var x in data) {
-	        var section_name = x;
-	        var section_data = data[x];
-	        
-	        if(typeof(section_data) == "object"){
-	            for (var d in section_data) {
-	                var itemName = d;
-	                var itemValue = section_data[d];
-	                newConf.AddSetting(section_name, itemName, itemValue);
-	                Util.ConsoleLog("  --  Adding \"" + itemName + "\" = \"" + itemValue + "\" to " + section_name + " section...", true);
-	            }
-	        } 
-	    }
-
-	    newConf.Save();
-
-	    return newConf;
-
-	} catch (err) {
-
-    	handleError("createConfig", err, plugin);
-
-    }
-}
-
-function loadConfig(){
-
-    try {
-
-    	var readConf = Plugin.GetIni(getFilename(plugin));
-    	return readConf;
-        
-    } catch (err) {
-    	handleError("loadConfig", err, plugin);
-    }
-}
-
-function handleError(funcName, err){
-	Util.ConsoleLog("Error logged in " + plugin.name + ".", true);
-	Plugin.Log("Error_log", "in " + funcName + " -- Error Message: " + err.message + " (" + plugin.name + ")");
-    if(err.description != undefined){
-        Plugin.Log("Error_log", "in " + funcName + " -- Error Description: " + err.description + " (" + plugin.name + ")")
-    }
-    Plugin.Log("Error_log", "-------------------------------------------------------------------------");
-}
-
-function confSetting(name, section) {
-	if(section == undefined){
-		section = "Config";
-	}
-	var conf = loadConfig();
-	return conf.GetSetting(section, name);
-}
-
-function sendData (data, method) {
-	
-    var server = DataStore.Get("BZ0core", "server");
-    var serverID = DataStore.Get("BZ0core", "serverID");
-    var serverPass = DataStore.Get("BZ0core", "serverPass");
-    var serverScript = DataStore.Get("BZ0core", "serverScript");
-
-    var chunk = "";
-    
-    for (var x in data) {
-		 var key = x;
-		 var value = data[x];
-		 chunk = chunk + "&" + key + "=" + value;
+	function getFilename(){
+		var filename = plugin.name;
+	    return filename.replace(" ", "_");
 	}
 
-	if(method == true){
-		var url= server + "/" + serverScript + "?serverID=" + serverID + "&pass=" + serverPass + chunk;
-		var request = Web.GET(url);
-		if(confSetting("admin_colsole_debug") == 1){
-			Util.ConsoleLog("sendData: " + url, true);
-		}
-	} else {
-		// default method it POST
-		var url= server + "/" + serverScript;
-		var chunk = "serverID=" + serverID + "&pass=" + serverPass + chunk;
-		var request = Web.POST(url, chunk);
-		if(confSetting("admin_colsole_debug") == 1){
-			Util.ConsoleLog("sendData: " + url + "[" + chunk + "]", true);
-		}
-	}
+	function createConfig(data){
+		try {
 
-	if(confSetting("admin_colsole_debug") == 1){
-		Util.ConsoleLog("sendData response: " + request, true);
-	}
-	return eval("(function(){return " + request + ";})()");
-}
+			var filename = getFilename(plugin);
 
-function bzCoreCheck(){
-	return 'loaded';
-}
+			Util.ConsoleLog("Config does not exist... creating " + filename + ".ini", true);
 
+		    Plugin.CreateIni(filename);
+		    var newConf = Plugin.GetIni(filename);
 
-
-function On_PluginInit() { 
-
-	if ( !Plugin.IniExists( getFilename() ) ) {
-
-        var Config = {};
-	        Config['global_chat_name'] = "Rustard";
-	        Config['admin_colsole_debug'] = 0;
-
-        var RemoteHost = {};
-	        RemoteHost['server'] = "http://rustard.com";
-			RemoteHost['serverID'] = 0;
-			RemoteHost['serverPass'] = "";
-			RemoteHost['serverScript'] = "logger";
-
-        var iniData = {};
-        	iniData["Config"] = Config;
-        	iniData["RemoteHost"] = RemoteHost;
-
-
-        var conf = createConfig(iniData);
-
-    } 
-
-    DataStore.Add("BZ0core", "server", confSetting("server", "RemoteHost"));
-    DataStore.Add("BZ0core", "serverID", confSetting("serverID", "RemoteHost"));
-    DataStore.Add("BZ0core", "serverPass", confSetting("serverPass", "RemoteHost"));
-    DataStore.Add("BZ0core", "serverScript", confSetting("serverScript", "RemoteHost"));
-
-    Server.server_message_name = confSetting("global_chat_name");    
-
-    Util.ConsoleLog(plugin.name + " loaded.", true);
-    
-}
-
-function On_ServerInit() { 
-	// meh
-}
-
-// Some things I have yet to rewrite for this:
-/*
-	// Use this for logging stuff -- demove after dev stage 
-	// Ideally have one function which data is sent to that lets you choose chat, broadcast, console or file log?
-
-
-	function dataDump (fileName, eventName, dataObj, indent) {
-		if(indent == undefined){
-			var indent = " - ";
-		}
-		Plugin.Log(fileName, eventName+": ");
-		Plugin.Log(fileName, "------");
-		for (var x in dataObj) {
-		     var output_name = x;
-			 var output_value = dataObj[x];
-			 if(typeof(output_value) != "function"){
-			 	Plugin.Log(fileName, indent + output_name + " : " + output_value);
-			 }
-		}
-		
-		Plugin.Log(fileName, "---------------------------------------------------------------------------------------------- ");
-	}
-
-	function dataWrite (Player, eventName, dataObj, indent) {
-		if(indent == undefined){
-			var indent = " - ";
-		}
-		Player.Message(eventName+": ");
-		Player.Message("------");
-		for (var x in dataObj) {
-		     var output_name = x;
-			 var output_value = dataObj[x];
-			 if(typeof(output_value) != "function"){
-			 	Player.Message(indent + output_name + " : " + output_value);
-			 }
-		}
-		
-		Player.Message("----------------------------------------");
-	}
-
-
-try {
-		    var check = checkCore();
-
-		    if(check != undefined){
-		    	Util.ConsoleLog("Core check from force passed: " + check, true);
-		    } else {
-		    	Util.ConsoleLog("Core check from force failed.", true);
+		    for (var x in data) {
+		        var section_name = x;
+		        var section_data = data[x];
+		        
+		        if(typeof(section_data) == "object"){
+		            for (var d in section_data) {
+		                var itemName = d;
+		                var itemValue = section_data[d];
+		                newConf.AddSetting(section_name, itemName, itemValue);
+		                Util.ConsoleLog("  --  Adding \"" + itemName + "\" = \"" + itemValue + "\" to " + section_name + " section...", true);
+		            }
+		        } 
 		    }
+
+		    newConf.Save();
+
+		    return newConf;
+
 		} catch (err) {
 
-	        Util.ConsoleLog("Error_log", "On_PluginInit -- Error Message: " + err.message, true);
-	        if(err.description != undefined){
-	        	Util.ConsoleLog("Error_log", "On_PluginInit -- Error Description: " + err.description, true)
-	        }
-	        Util.ConsoleLog("Error_log", "-------------------------------------------------------------------------", true);
+	    	handleError("createConfig", err, plugin);
+
+	    }
+	}
+
+	function loadConfig(){
+
+	    try {
+
+	    	var readConf = Plugin.GetIni(getFilename(plugin));
+	    	return readConf;
 	        
+	    } catch (err) {
+	    	handleError("loadConfig", err, plugin);
+	    }
+	}
+
+	function handleError(funcName, err){
+		Util.ConsoleLog("Error logged in " + plugin.name + ".", true);
+		Plugin.Log("Error_log", "in " + funcName + " -- Error Message: " + err.message + " (" + plugin.name + ")");
+	    if(err.description != undefined){
+	        Plugin.Log("Error_log", "in " + funcName + " -- Error Description: " + err.description + " (" + plugin.name + ")")
+	    }
+	    Plugin.Log("Error_log", "-------------------------------------------------------------------------");
+	}
+
+	function confSetting(name, section) {
+		if(section == undefined){
+			section = "Config";
+		}
+		var conf = loadConfig();
+		return conf.GetSetting(section, name);
+	}
+
+	function sendData (data, method) {
+		
+	    var server = DataStore.Get("BZ0core", "server");
+	    var serverID = DataStore.Get("BZ0core", "serverID");
+	    var serverPass = DataStore.Get("BZ0core", "serverPass");
+	    var serverScript = DataStore.Get("BZ0core", "serverScript");
+
+	    if(serverID == 0){
+	    	Util.ConsoleLog("RemoteHost config is still set to default. Not sending.", true);
+	    	return false;
 	    }
 
-*/
+	    var chunk = "";
+
+	    for (var x in data) {
+			 var key = x;
+			 var value = data[x];
+			 chunk = chunk + "&" + key + "=" + value;
+		}
+
+		if(method == true){
+			var url= server + "/" + serverScript + "?serverID=" + serverID + "&pass=" + serverPass + chunk;
+			var request = Web.GET(url);
+			if(confSetting("admin_colsole_debug") == 1){
+				Util.ConsoleLog("sendData: " + url, true);
+			}
+		} else {
+			// default method it POST
+			var url= server + "/" + serverScript;
+			var chunk = "serverID=" + serverID + "&pass=" + serverPass + chunk;
+			var request = Web.POST(url, chunk);
+			if(confSetting("admin_colsole_debug") == 1){
+				Util.ConsoleLog("sendData: " + url + "[" + chunk + "]", true);
+			}
+		}
+
+		if(confSetting("admin_colsole_debug") == 1){
+			Util.ConsoleLog("sendData response: " + request, true);
+		}
+		return eval("(function(){return " + request + ";})()");
+	}
+
+	function bzCoreCheck(){
+
+		return 'loaded';
+	}
+
+// Plugin stuff:
+
+	function On_PluginInit() { 
+
+		if ( !Plugin.IniExists( getFilename() ) ) {
+
+	        var Config = {};
+		        Config['global_chat_name'] = "Rustard";
+		        Config['admin_colsole_debug'] = 0;
+
+	        var RemoteHost = {};
+		        RemoteHost['server'] = "notset";
+				RemoteHost['serverID'] = 0;
+				RemoteHost['serverPass'] = "notset";
+				RemoteHost['serverScript'] = "notset";
+
+	        var iniData = {};
+	        	iniData["Config"] = Config;
+	        	iniData["RemoteHost"] = RemoteHost;
+
+
+	        var conf = createConfig(iniData);
+
+	    } 
+
+	    DataStore.Add("BZ0core", "server", confSetting("server", "RemoteHost"));
+	    DataStore.Add("BZ0core", "serverID", confSetting("serverID", "RemoteHost"));
+	    DataStore.Add("BZ0core", "serverPass", confSetting("serverPass", "RemoteHost"));
+	    DataStore.Add("BZ0core", "serverScript", confSetting("serverScript", "RemoteHost"));
+
+	    Server.server_message_name = confSetting("global_chat_name");    
+
+	    Util.ConsoleLog(plugin.name + " loaded.", true);
+	    
+	}
+
+	function On_ServerInit() { 
+		var data = {};
+		data['action'] = "ServerInit";
+		var response = {};
+		response = sendData(data);
+	}
+
+
+
+// Some things I have yet to rewrite for this:
+	/*
+		// Use this for logging stuff -- demove after dev stage 
+		// Ideally have one function which data is sent to that lets you choose chat, broadcast, console or file log?
+		// also check debug setting? perhaps add debug level?
+
+
+		function dataDump (fileName, eventName, dataObj, indent) {
+			if(indent == undefined){
+				var indent = " - ";
+			}
+			Plugin.Log(fileName, eventName+": ");
+			Plugin.Log(fileName, "------");
+			for (var x in dataObj) {
+			     var output_name = x;
+				 var output_value = dataObj[x];
+				 if(typeof(output_value) != "function"){
+				 	Plugin.Log(fileName, indent + output_name + " : " + output_value);
+				 }
+			}
+			
+			Plugin.Log(fileName, "---------------------------------------------------------------------------------------------- ");
+		}
+
+		function dataWrite (Player, eventName, dataObj, indent) {
+			if(indent == undefined){
+				var indent = " - ";
+			}
+			Player.Message(eventName+": ");
+			Player.Message("------");
+			for (var x in dataObj) {
+			     var output_name = x;
+				 var output_value = dataObj[x];
+				 if(typeof(output_value) != "function"){
+				 	Player.Message(indent + output_name + " : " + output_value);
+				 }
+			}
+			
+			Player.Message("----------------------------------------");
+		}
+
+
+	try {
+			    var check = checkCore();
+
+			    if(check != undefined){
+			    	Util.ConsoleLog("Core check from force passed: " + check, true);
+			    } else {
+			    	Util.ConsoleLog("Core check from force failed.", true);
+			    }
+			} catch (err) {
+
+		        Util.ConsoleLog("Error_log", "On_PluginInit -- Error Message: " + err.message, true);
+		        if(err.description != undefined){
+		        	Util.ConsoleLog("Error_log", "On_PluginInit -- Error Description: " + err.description, true)
+		        }
+		        Util.ConsoleLog("Error_log", "-------------------------------------------------------------------------", true);
+		        
+		    }
+
+	*/
---
+++ BZusers/BZusers.js
@@ -0,0 +1,183 @@
+// User tools for remote and offline data
+
+var plugin = {};
+	plugin.name = "BZusers";
+	plugin.author = "BadZombi";
+	plugin.version = "0.4";
+
+
+// support functions:
+
+	function playersonlineCallback () {
+		try{
+			var players = Server.Players;
+			var online = players.Count;
+					
+			
+			var data = {};
+			data['action'] = "update_online";
+
+
+			if(online >= 1){
+				var count = 0;
+				var playerData = "{";
+				for (var x in players){
+					count++;
+					playerData += '"' + count + '":"' + x.SteamID+ '"';
+					if(count != online){
+						playerData += ',';
+					}	
+				}
+				playerData += "}";
+
+				data['players'] = playerData;
+			} else {
+				data['players'] = 'nope';
+			}
+				
+			var response = {};
+			response = sendData(data);
+				
+		} catch (err) {
+			handleError("playersonlineCallback", err);
+	    }
+	}
+
+// Name filter thanks to Shawn Mueller (Fe2O3NameFilter)
+
+	function ContainsOnlyUSKeyboardChars(str){
+		return str.match(/^[a-zA-Z0-9 '\`\?\~<>{}|:;,\/!@#\$%\^\&*\[\]\)\(+=._-]+$/g);
+	}
+
+	function FilterNonUSKeyboardChars(str){
+		var new_str = "";
+		for(var i = 0; i < Data.StrLen(str);i++){
+			var chr = Data.Substring(str, i, 1);
+			if(ContainsOnlyUSKeyboardChars(chr)){
+				new_str = new_str + chr;
+			}
+		}
+		return new_str;
+	}
+
+	function FilterLeadingWhitespace(str){
+		var new_str = "";
+		var first_non_ws_found = false;
+		for(var i = 0; i < Data.StrLen(str);i++){
+			var chr = Data.Substring(str, i, 1);
+			if(chr != " "){
+				first_non_ws_found = true;
+			}
+			if(first_non_ws_found){
+				new_str = new_str + chr;
+			}
+		}
+		
+		return new_str;
+	}
+
+	function FilterTrailingWhitespace(str){
+		var new_str = "";
+		var first_non_ws_found = false;
+		for(var i = 0; i < Data.StrLen(str);i++){
+			var chr = Data.Substring(str, Data.StrLen(str)-i-1, 1);
+			if(chr != " "){
+				first_non_ws_found = true;
+			}
+			if(first_non_ws_found){
+				new_str = chr + new_str;
+			}
+		}
+
+		return new_str;
+	}
+
+	function FixName(str){
+		var new_str = FilterNonUSKeyboardChars(str);
+		new_str = FilterLeadingWhitespace(new_str);
+		new_str = FilterTrailingWhitespace(new_str);
+		if(Data.StrLen(new_str) == 0){
+			new_str = "InvalidName"+Math.floor(Math.random()*10000);
+			Player.Message("Your name is invalid.");
+			Player.Message("You wont be allowed to connect until you change it.");
+			Player.Disconnect();	
+		}
+		return new_str;
+	}
+
+// main plugin stuff:
+
+	function On_PluginInit() { 
+
+		if(bzCoreCheck() != 'loaded'){
+	        Util.ConsoleLog("Could not load " + plugin.name + "! (Zero Core not loaded yet)", true);
+	        return false;
+	    }
+
+	    if ( !Plugin.IniExists( getFilename() ) ) {
+
+	        var Config = {};
+	        	Config['send_online_to_web'] = 0;
+		        Config['poll_time_in_seconds'] = 10;
+		        Config['filter_usernames'] = 1;
+
+	        var iniData = {};
+	        	iniData["Config"] = Config;
+
+	        var conf = createConfig(iniData);
+
+	    } 
+
+	    Util.ConsoleLog(plugin.name + " plugin loaded.", true);
+
+	    if(confSetting("send_online_to_web") == 1){
+	    	Plugin.CreateTimer("playersonline", confSetting("poll_time_in_seconds") * 1000).Start();
+	    	Util.ConsoleLog(" -- Users online being sent to web every " + confSetting("poll_time_in_seconds") + " seconds.", true);
+	    }
+	}
+
+	function On_PlayerConnected(Player){
+		
+		if(confSetting("filter_usernames") == 1){
+
+			// Fix user names thanks to Shawn Mueller (Fe2O3NameFilter)
+			if (!ContainsOnlyUSKeyboardChars(Player.Name)){
+				var fixed_name = FixName(Player.Name);
+				Player.Name = fixed_name;
+			}
+		}
+
+		// save steamID -> name in datastore for offline usage
+		try{
+			DataStore.Add(Player.SteamID, "BZName", Player.Name);
+			DataStore.Save();
+		} catch(err) {
+			handleError("On_PlayerConnected", err);
+		}
+
+		// log connection status to website:
+		if(confSetting("send_online_to_web") == 1){
+			var data = {};
+			data['action'] = "connect";
+			data['name'] = Player.Name;
+			data['sid'] = Player.SteamID;
+			data['ip'] = Player.IP;
+
+			var response = {};
+			response = sendData(data);
+		}
+
+	}
+
+	function On_PlayerDisconnected(Player){	
+		// log Disconnect to website
+		if(confSetting("send_online_to_web") == 1){
+			locator(Player);
+
+			var data = {};
+			data['action'] = "disconnect";
+			data['sid'] = Player.SteamID;
+			sendData(data);
+		}
+	}
+
